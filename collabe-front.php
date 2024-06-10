f561c25b13f2ada4a39d2d4efd07d3bcb8ec83dd
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-06-10 06:32:05 +0530

    collage
    start

---
+++ collabe-front.php
@@ -0,0 +1,31 @@
+<?php
+include("config.php");
+$result = mysqli_query($mysqli, "SELECT * from front");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Life in AMU</title>
+    <link rel="stylesheet" href="style/collabe.css">
+</head>
+<body>
+
+    <header class="header">
+        <h1>Student Life in Aligarh Muslim University</h1>
+    </header>
+    <div class="card-container"> 
+    <?php
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<div class='card'>";
+        echo "<img src='/" . $row['image'] . "' style='height: 165px;' class='card-image'>"; 
+        echo "<h3><a href='index.php'>" . $row['heading'] . "</a></h3>";
+        echo "<div class='card-description'>" . $row['description'] . "</div>";
+        echo "</div>";
+    }
+    ?>
+    </div> 
+</body>
+</html>
